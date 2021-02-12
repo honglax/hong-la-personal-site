b42d75464e68ab0cc7546653f4fb94f986ca2ebb
Hong La <hongla@kms-technology.com>
2021-02-12 16:05:24 +0700

    Implement localization

---
+++ src/i18n/config.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import translationEN from './locales/en/translation.json'
+import translationVI from './locales/vi/translation.json'
+import { initReactI18next } from 'react-i18next'
+
+export const resources = {
+  en: {
+    translation: translationEN,
+  },
+  vi: {
+    translation: translationVI,
+  },
+} as const
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  debug: true,
+  resources,
+})
